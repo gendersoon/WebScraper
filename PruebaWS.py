14a174f1038a89acb84c7fbc9a429e097526ed44
icarreons <akatsuki96_ivan@hotmail.com>
2017-11-26 20:15:04 -0600

    pruebai4

--- PruebaWS.py
+++ PruebaWS.py
@@ -7,12 +7,12 @@ class Testscraper(unittest.TestCase):
     def setUp(self):
        self.m = Main
 
-     #~ Probamos la aplicación con una página de stackoverflow, al ejecutarse correctamente devuelve None
+     
     def test_scraper(self):
         self.assertIsNone(self.m.main(self,"https://stackoverflow.com/questions/16840409/how-to-list-containers-in-docker"))
 
 
-    #~ Cuando enviamos una página incorrecta nos arroja el error AttributeError
+    
     def test_attr_error(self):
         with self.assertRaises(AttributeError):
             self.m.main(self, "https://docs.python.org/3/library/unittest.html")
